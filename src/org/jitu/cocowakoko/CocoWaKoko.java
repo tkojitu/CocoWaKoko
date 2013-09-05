915977834fc0589ee2ffcd6848d41773166b6ded
tko <tko@muba.jitu.org>
2013-09-05 22:32:17 +0900

    public beta

--- src/org/jitu/cocowakoko/CocoWaKoko.java
+++ src/org/jitu/cocowakoko/CocoWaKoko.java
@@ -1,5 +1,12 @@
 package org.jitu.cocowakoko;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesClient;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -8,6 +15,7 @@ import com.google.android.gms.location.LocationClient;
 import android.location.Location;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.IntentSender;
@@ -19,13 +27,39 @@ public class CocoWaKoko extends Activity implements
         GooglePlayServicesClient.ConnectionCallbacks,
         GooglePlayServicesClient.OnConnectionFailedListener {
     private LocationClient client;
-    private Location location;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (!makeAppHome()) {
+            finish();
+        }
         client = new LocationClient(this, this, this);
         setContentView(R.layout.activity_coco_wa_koko);
+    }
+
+    private boolean makeAppHome() {
+        File home = getAppHome();
+        if (!home.exists()) {
+            if (!home.mkdir()) {
+                Toast.makeText(this, "cannot make app home", Toast.LENGTH_LONG).show();
+                return false;
+            }
+        }
+        if (!home.isDirectory()) {
+            Toast.makeText(this, "app home is not a directory", Toast.LENGTH_LONG).show();
+            return false;
+        }
+        return true;
+    }
+
+    private File getAppHome() {
+        File storage = Environment.getExternalStorageDirectory();
+        return new File(storage, getAppName());
+    }
+
+    private String getAppName() {
+        return getResources().getString(R.string.app_name);
     }
 
     @Override
@@ -47,31 +81,80 @@ public class CocoWaKoko extends Activity implements
     }
 
     public void onClickCocoWaDoko(View view) {
-        if (servicesConnected()) {
-            location = client.getLastLocation();
-            String msg = (location == null) ? "location is null" : location.toString();
-            Toast.makeText(this, msg, Toast.LENGTH_LONG).show();
-        }
-    }
-
-    public void onClickSendLocation(View view) {
-        Uri uri = getLocationUri();
-        if (uri == null) {
-            Toast.makeText(this, "location is null", Toast.LENGTH_LONG).show();
+        if (!servicesConnected()) {
+            Toast.makeText(this, "not connected to GPS", Toast.LENGTH_LONG).show();
             return;
         }
-        startMap(uri);
+        Location loc = client.getLastLocation();
+        if (loc == null) {
+            Toast.makeText(this, "location is not available", Toast.LENGTH_LONG).show();
+            return;
+        }
+        String uriStr = getLocationUriString(loc);
+        if (!storeUriString(uriStr)) {
+            return;
+        }
+        startMap(Uri.parse(uriStr));
     }
 
-    private Uri getLocationUri() {
-        if (location == null) {
-            return null;
+    private boolean storeUriString(String uriStr) {
+        File file = getUriFile();
+        return saveFile(file, uriStr);
+    }
+
+    public boolean saveFile(File file, String text) {
+        try {
+            FileWriter wf = new FileWriter(file, true);
+            BufferedWriter bw = new BufferedWriter(wf);
+            bw.write(text, 0, text.length());
+            bw.newLine();
+            bw.close();
+            return true;
+        } catch (IOException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            return false;
         }
+    }
+
+    private File getUriFile() {
+        return new File(getAppHome(), "uri.txt");
+    }
+
+    private String getLocationUriString(Location loc) {
         StringBuilder buf = new StringBuilder("geo:0,0?q=");
-        buf.append(location.getLatitude())
+        buf.append(loc.getLatitude())
             .append(",")
-            .append(location.getLongitude());
-        return Uri.parse(buf.toString());
+            .append(loc.getLongitude());
+        return buf.toString();
+    }
+
+    public void onClickShowMap(View view) {
+        File file = getUriFile();
+        String uriStr = readUriStr(file);
+        if (uriStr == null) {
+            return;
+        }
+        startMap(Uri.parse(uriStr));
+    }
+
+    private String readUriStr(File file) {
+        String uriStr = null;
+        try {
+            FileReader fr = new FileReader(file);
+            BufferedReader bf = new BufferedReader(fr);
+            try {
+                String line;
+                while ((line = bf.readLine()) != null) {
+                    uriStr = line;
+                }
+                return uriStr;
+            } finally {
+                bf.close();
+            }
+        } catch (IOException e) {
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            return uriStr;
+        }
     }
 
     private void startMap(Uri uri) {
